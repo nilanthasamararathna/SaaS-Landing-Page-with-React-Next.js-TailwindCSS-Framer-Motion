83054767878163f8eacc64b313f10daf9d2ad63c
E.Nilantha Samararathna <45939807+nilanthasamararathna@users.noreply.github.com>
2025-07-05 21:14:40 +0530

    create company logos section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,12 @@
 import ArrowIcon from '@/assets/arrow-right.svg';
 import cogImage from '@/assets/cog.png';
+import cylinderImage from '@/assets/cylinder.png';
+import noodleImage from '@/assets/noodle.png';
 import Image from 'next/image';
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
           <div className='md:w-[478px]'>
@@ -27,7 +29,9 @@ export const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image src={cogImage} alt="Cog Image" className='absolute md:h-full md:w-auto md:max-w-none md:-left-6' />
+            <Image src={cogImage} alt="Cog Image" className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0' />
+            <Image src={cylinderImage} width={220} height={220} alt="Cylinder Image" className='hidden md:block -top-8 -left-32 md:absolute' />
+            <Image src={noodleImage} width={220} alt='Noodle Image' className='hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]' />
           </div>
         </div>
       </div>
